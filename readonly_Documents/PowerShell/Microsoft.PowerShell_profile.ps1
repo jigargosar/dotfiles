fcb14a7e29b70c7919ef41bca0437af0626abeec
Jigar Gosar <jigar.gosar@gmail.com>
2026-01-08 12:32:32 +0530

    Update PowerShell profile

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -6,3 +6,4 @@ Invoke-Expression (& { (chezmoi completion powershell | Out-String) })
 # oh-my-posh init pwsh | Invoke-Expression
 
 # $Env:EDITOR = "notepad.exe"
+Set-PSReadLineKeyHandler -Key Ctrl+w -Function UnixWordRubout
